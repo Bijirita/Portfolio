8e6f2b907a8e33a70778ad02532b788523893577
Bijirita <bijirita009@gmail.com>
2018-12-03 15:57:41 -0500

    test is progress

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+const nav = {
+    aboutMe: document.getElementById('aboutme'),
+    gitHub: document.getElementById('github'),
+    faceBook: document.getElementById('facebook'),
+    linkedIn: document.getElementById('linkedin'),
+    contactMe: document.getElementById('contactme')
+}
+
