12be32627c782e77d440a30d455019ce9afdbe7e
Bijirita <bijirita009@gmail.com>
2018-12-05 16:17:34 -0500

    form validation

--- contact/contactjs.js
+++ contact/contactjs.js
@@ -1,38 +1,41 @@
 var alertRedInput = "#8C1010";
 var defaultInput = "rgba(10, 180, 180, 1)";
 
-function userNameValidation(usernameInput) {
-    var username = document.getElementById("username");
+function nameValidation(nameInput) {
+    var name = document.getElementById("name");
     var issueArr = [];
-    if (/[-!@#$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/.test(usernameInput)) {
-        issueArr.push("No special characters!");
+    if (/[-!@#$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/.test(nameInput)) {
+        name.setCustomValidity("No special characters!");
+        name.style.borderColor = alertRedInput;
     }
-    if (issueArr.length > 0) {
-        username.setCustomValidity(issueArr);
-        username.style.borderColor = alertRedInput;
-    } else {
-        username.setCustomValidity("");
-        username.style.borderColor = defaultInput;
+    else {
+        name.setCustomValidity("");
+        name.style.borderColor = defaultInput;
     }
 }
 
-function passwordValidation(passwordInput) {
-    var password = document.getElementById("password");
+function validateEmail(emailInput) {
+    var email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return email.test(String(emailInput).toLowerCase());
+}
+
+function emailValidation(emailInput) {
+    var email= document.getElementById("email");
     var issueArr = [];
-    if (!/^.{7,15}$/.test(passwordInput)) {
-        issueArr.push("Password must be between 7-15 characters.");
+    if (!/^.{7,15}$/.test(emailInput)) {
+        issueArr.push("Please submit valid email.");
     }
-    if (!/\d/.test(passwordInput)) {
+    if (!/\d/.test(emailInput)) {
         issueArr.push("Must contain at least one number.");
     }
-    if (!/[a-z]/.test(passwordInput)) {
+    if (!/[a-z]/.test(emailInput)) {
         issueArr.push("Must contain a lowercase letter.");
     }
-    if (!/[A-Z]/.test(passwordInput)) {
+    if (!/[A-Z]/.test(emailInput)) {
         issueArr.push("Must contain an uppercase letter.");
     }
     if (issueArr.length > 0) {
-        password.setCustomValidity(issueArr.join("\n"));
+        email.setCustomValidity(issueArr.join("\n"));
         password.style.borderColor = alertRedInput;
     } else {
         password.setCustomValidity("");
